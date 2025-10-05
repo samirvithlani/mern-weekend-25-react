b82de9224ba5613bac263db15377b9b2306a5918
samirvithlani <samir.vithlani83955@gmail.com>
2025-10-05 12:55:58 +0530

    task

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 export const UserList = (props) => {
     //{data:""}
     //{data:"",user:{}}
+    //{data:"",user:{},test:f}
     console.log("userlist prop",props)
   return (
     <div style={{color:"blue"}}>
+      
         <h1>USER LIST</h1>
         <h5>{props.data}</h5>
         <h6>{props.user.name}</h6>
         <h6>{props.user.id}</h6>
         <h6>{props.user.age}</h6>
+        <button onClick={()=>{props.test()}}>TEST</button>
+        <button onClick={()=>{props.test1(100)}}>TEST 1</button>
     </div>
   )
 }
+
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -7,12 +7,20 @@ export const Users = () => {
     id:101,
     name:"ram",
     age:23
-
   }
+  const test =()=>{
+    alert("test function called....")
+  }
+
+  const test1=(x)=>{
+    alert("test1 called "+x)
+  }
+
   return (
     <div style={{textAlign:"center"}}>
         <h1>USER COMPONNET</h1>
-        <UserList data = {data} user = {user}></UserList>
+        
+        <UserList data = {data} user = {user} test ={test} test1={test1}></UserList>
     </div>
   )
 }
