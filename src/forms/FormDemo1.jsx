eca7604648855ad180c8909b1d50fd61be4c552e
samirvithlani <samir.vithlani83955@gmail.com>
2025-10-26 12:09:27 +0530

    form

--- src/forms/FormDemo1.jsx
+++ src/forms/FormDemo1.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 export const FormDemo1 = () => {
 
     const {register,handleSubmit} =useForm()
+    const [output, setoutput] = useState({})
+    const [isSubbmited, setisSubbmited] = useState(false)
     //regisger is used form regiseter input fileds
     //handle submit is used for handle submit event..
 
     //USer difine function..
     const submitHandler = (data)=>{
-        console.log(data)
+        console.log(data) //{name:""}
+        setoutput(data)
+        setisSubbmited(true)
     }
 
   return (
@@ -25,9 +29,21 @@ export const FormDemo1 = () => {
                 <input type='text' {...register("age")}></input>
             </div>
             <div>
+                <label>COLOR</label>
+                <input type='color' {...register("color")}></input>
+            </div>
+            <div>
                 <input type='submit'></input>
             </div>
         </form>
+        {
+            isSubbmited == true &&  <div style={{color:output.color}}>
+            <h1>OUTPUT</h1>
+            <h2>Name = {output.name}</h2>
+            <h3>AGe = {output.age}</h3>
+        </div>
+        }
+        
     </div>
   )
 }
