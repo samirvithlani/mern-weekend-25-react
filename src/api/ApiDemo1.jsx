7090fe423534bf2e233c76b8aac9c87b09d0894c
samirvithlani <samir.vithlani83955@gmail.com>
2025-11-29 17:29:25 +0530

    delet apie

--- src/api/ApiDemo1.jsx
+++ src/api/ApiDemo1.jsx
@@ -10,7 +10,7 @@ export const ApiDemo1 = () => {
 
         //URL -->API
         //Promise<AxiosResponse<any>
-        const res = await axios.get("https://node5.onrender.com/user/user")
+        const res = await axios.get("https://node5.onrender.com/user/user") //updated record...
         console.log("response axios object",res)
         //to access api response u need to call data variable from res
         console.log("api response...",res.data)
@@ -21,11 +21,52 @@ export const ApiDemo1 = () => {
         //table formate..
 
     }
+
+    const deleteUser = async(id)=>{
+      //id == userId
+      //alert(id)
+      //delete api call
+      //REUEST METGOD DLEETE https://node5.onrender.com/user/user/{userid}
+      const res = await axios.delete(`https://node5.onrender.com/user/user/${id}`)
+      console.log(res)
+      if(res.status==204){
+        alert("user deleted successfully!!")
+        //get API CALL --> database updated..
+        getUsers()
+      }
+    }
   return (
     <div style={{textAlign:"center"}}>
         <h1>API DEMO 1</h1>
         <button onClick={getUsers}>GET</button>
-        {message}
+        <table className='table table-dark'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>AGE</th>
+              <th>EMAIL</th>
+              <th>STATUS</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users?.map((user)=>{
+                return<tr>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.age}</td>
+                  <td>{user.email}</td>
+                  <td>{user.isActive == true?"true":"false"}</td>
+                  <td>
+                    <button className='btn btn-danger' onClick={()=>{deleteUser(user._id)}}>DELETE</button>
+                  </td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
     </div>
   )
 }
